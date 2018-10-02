7eb4f6eeb862651febf076badadd21405048b901
Roque Francisco <roquefsd@gmail.com>
2018-10-02 00:44:11 -0300

    winning

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var running = false;
 
 let red = document.getElementById('red');
 let blue = document.getElementById('blue');
@@ -8,18 +9,31 @@ function calculateColor(mouse) {
     let color = 255 * percentage / 100;
     red.style.background = `rgba(255, 181, 181, ${percentage / 100})`;
     blue.style.background = `rgba(194, 244, 255, ${1 - (percentage / 100)})`;
-    
-    if(percentage >= dificulty) {
-        alert((new Date() - init_time) / 1000 + 's');
+
+    if (percentage >= dificulty) {
         createWinDiv();
-        init();
+        running = false;
     }
+}
+
+let radius = 70;
+function createWinDiv() {
+    let winning_div = $(`<div class="circle" onclick="win();" style="left: ${point.x - radius}px; top: ${point.y - radius}px"></div>`);
+    $('.circle').remove();
+    $('body').append(winning_div);
+    setTimeout(() => {
+        winning_div.addClass('active');
+        $('#red').addClass('normal');
+        $('#blue').addClass('normal');
+    }, 0);
 }
 
 function listeners() {
     $('body').mousemove((e) => {
-        calculateColor({ x: e.pageX, y: e.pageY });
-    })
+        if(running) {
+            calculateColor({ x: e.pageX, y: e.pageY });
+        }
+    });
 }
 
 let point;
@@ -40,10 +54,18 @@ function distance(p1, p2) {
 let maxDistance = distance({ x: 0, y: 0 }, { x: window.innerWidth, y: window.innerHeight });
 let init_time;
 function init() {
+    running = true;
     init_time = new Date();
-    dificulty += (100 - dificulty) / 4; 
+    dificulty += (100 - dificulty) / 4;
     newGame();
     listeners();
+}
+
+function win() {
+    let time = new Date(new Date() - init_time);
+    time = time.getSeconds() + 's';
+    $('.js-time').text(time);
+
 }
 
 init();
